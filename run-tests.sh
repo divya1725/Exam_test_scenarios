93192e497f26bdd8ae093fd5062586ed075a0aae
Ullasa Srinivasa <ullasa.srinivasa@evry.com>
2020-09-01 15:37:30 +0200

    run-tests.sh edited online with Bitbucket
    shell dscript to loop through all dir in project

--- run-tests.sh
+++ run-tests.sh
@@ -5,15 +5,14 @@ ENV=$1
 
 echo "LOCALDIR=$LOCALDIR and EnvironemntName is $ENV"
 
-# Declare an array of string with type
-declare -a projectArray=("FraudPayments" "InspectionLogging" "PreDefined-Creditor")
- 
-# Iterate the string array using for loop
-for val in ${projectArray[@]}; do
-  echo "values is $val"
-  docker run -v="$LOCALDIR/$val":/project -v="$LOCALDIR/$val/reports":/reports -v="$LOCALDIR/ext":/ext/ \
-    -e LICENSE_SERVER="fslicense.evry.com:1099" \
-    -e COMMAND_LINE="-f/%reports% '-RJUnit-Style HTML Report' -FHTML '-E$ENV' '/%project%/' "  \
-     fsnexus.evry.com:8085/smartbear/ready-api-soapui-testrunner:3.1.0
+for project in */ ; do
+    echo " Outside foldeer $project"
+	if [ "$project" != "ext/" ]
+	then
+		echo "run Composite Project $project"
+        docker run -v="$LOCALDIR/$project":/project -v="$LOCALDIR/$project/reports":/reports -v="$LOCALDIR/ext":/ext/ \
+          -e LICENSE_SERVER="fslicense.evry.com:1099" \
+          -e COMMAND_LINE="-f/%reports% '-RJUnit-Style HTML Report' -FHTML '-E$ENV' '/%project%/' "  \
+           fsnexus.evry.com:8085/smartbear/ready-api-soapui-testrunner:3.1.0
+	fi
 done
-

7040bd21841b32c41482e84c26cb53d4656990d1
Ullasa Srinivasa <ullasa.srinivasa@evry.com>
2020-09-01 12:26:38 +0200

    run-tests.sh edited online with Bitbucket
    updated run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -4,8 +4,4 @@ LOCALDIR=$PWD
 
 echo "LOCALDIR=$LOCALDIR"
 
-docker run -v=$LOCALDIR/FraudPayments/:/project/ -v=$LOCALDIR/project/scripts/:/scripts/ \
-  -v=$LOCALDIR/FraudPayments/reports/:/reports/ \
-  -e LICENSE_SERVER="fslicense.evry.com:1099" \
-  -e COMMAND_LINE="'/%project%/'" \
-  fsnexus.evry.com:8085/smartbear/ready-api-soapui-testrunner:3.1.0
+docker run -v="${LOCALDIR}/${project}":/project -v="${LOCALDIR}/${project}/reports":/reports -v="${LOCALDIR}/ext":/ext/ -e LICENSE_SERVER="fslicense.evry.com:1099" -e COMMAND_LINE="-f/%reports% '-RJUnit-Style HTML Report' -FHTML '-E${params.Environments}' '/%project%/' "  fsnexus.evry.com:8085/smartbear/ready-api-soapui-testrunner:3.1.0

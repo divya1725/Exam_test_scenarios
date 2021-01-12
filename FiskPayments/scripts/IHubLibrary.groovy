3d052777a092999a3c82a7960dae99d84ca1a562
e232517 <ullasa.srinivasa@evry.com>
2021-01-12 19:03:50 +0530

    NOJIRA: Added POR step to FISK payments

--- FiskPayments/scripts/IHubLibrary.groovy
+++ FiskPayments/scripts/IHubLibrary.groovy
@@ -2,11 +2,9 @@ import java.text.DecimalFormat;
 
 class IHubLibrary{
 
-	public static Map<String,String> map = ['daily':'Daglig','weekly':'Uke','biweekly':'14.dag','semimonthly':'2 g mnd','monthly':'Måned','bimonthly':'2. mnd','quarterly':'Kvartal','tertially':'Tertial','semiannually':'Halvår','yearly':'Årlig']
+	public static Map<String,String> map = ['daily':'Daglig','weekly':'Uke','biweekly':'14.dag','semimonthly':'2 g mnd','monthly':"${new String([77, -61, -91, 110, 101, 100] as byte[])}",'bimonthly':'2. mnd','quarterly':'Kvartal','tertially':'Tertial','semiannually':"${new String([72, 97, 108, 118, -61, -91, 114] as byte[])}",'yearly':"${new String([-61, -123, 114, 108, 105, 103] as byte[])}"]
 	
 	public static String validatePeriodicIHubText(amount,ccyCode,creditAccount,firstDueDate,recurrenceType,log){
-
-
 		def amountStr = getAmountString(amount.toString())
 		
 		if(ccyCode.toUpperCase() !=  "NOK")
@@ -18,37 +16,34 @@ class IHubLibrary{
 			if (creditAccount.size() >11)
 				creditAccount = 'UTLAND'
 
-			Date d = Date.parse('yyyy-MM-dd',firstDueDate)
-			def sdf = new java.text.SimpleDateFormat ("ddMMyy")
-			def newDueDate = sdf.format(d).toString()
-
+			
+			def newDueDate = java.time.LocalDate.parse(firstDueDate).format(java.time.format.DateTimeFormatter.ofPattern("ddMMyy")).toString()
+			log.info newDueDate
 			def newrecurrenceType = map.get(recurrenceType.toLowerCase())
-
+			//newrecurrenceType = new String(newrecurrenceType.getBytes(), "UTF-8");		
 			if(ccyCode == "") 
 				return [amountStr ,creditAccount,"Forf:" +newDueDate, newrecurrenceType].join(" ")
 			else
 				return [amountStr ,ccyCode,creditAccount,"Forf:" +newDueDate, newrecurrenceType].join(" ")
 			
-			
 	}
 
 	public static def validateBalanceIHubText(accountNumber,creditAccountNumber,firstDueDate,recurrenceType,limitType,log){
 
-		def hentOrOver = 'Hent'
+		String hentOrOver = "Hent"
+		def hentOrOverArray 
 		if(limitType.toString().equalsIgnoreCase('ABOVE')){
-			accountNumber = creditAccountNumber
-			hentOrOver = "Overfør"
-		}
-
+			accountNumber = creditAccountNumber			
+			hentOrOverArray = [79, 118, 101, 114, 102, -61, -72, 114] as byte[]
+			hentOrOver = new String(hentOrOverArray, "UTF-8");
+		}	
+		
 		if (accountNumber.size() >11)
 					accountNumber = 'UTLAND'
 
-		Date d = Date.parse('yyyy-MM-dd',firstDueDate)
-		def sdf = new java.text.SimpleDateFormat ("ddMMyy")
-		def newDueDate = sdf.format(d).toString()
+		def newDueDate = java.time.LocalDate.parse(firstDueDate).format(java.time.format.DateTimeFormatter.ofPattern("ddMMyy")).toString()
 
-		def newrecurrenceType = map.get(recurrenceType.toLowerCase())					
-
+		def newrecurrenceType = map.get(recurrenceType.toLowerCase())
 		return[accountNumber ,"Forf:" +newDueDate, newrecurrenceType, hentOrOver].join(" ")
 		
 	}

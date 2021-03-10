f3a994ee8f2ff8588e040424d35984cb4c8bc728
e232517 <ullasa.srinivasa@evry.com>
2021-03-10 13:01:27 +0530

    NOJIRA: Env specific json file

--- scripts/ProjectEnvironments.groovy
+++ scripts/ProjectEnvironments.groovy
@@ -6,30 +6,30 @@ import com.eviware.soapui.model.environment.EndpointImpl
 class ProjectEnvironments {
 
     public static def log = null
-	public static def context = null
+    public static def context = null
 
     public static void load(def testRunner,def context,  def log){
         this.log=log;
-		this.context = context;
+        this.context = context;
         def project = testRunner.getProject();
-		log.info "Scripts started"
+        log.info "Scripts started"
         def envActive = project.getActiveEnvironment()
         def envActiveName = envActive.getName();
         def allEnvList = project.getEnvironmentList();
         def constantEnv = "G-D4"
         def actualEnv = System.getenv().get("COMMAND_LINE"); // get actual environment from user input
-		//actualEnv = "G-D6"
+        //actualEnv = "G-D6"
         log.info "Actual envirnment from user input is -- $actualEnv"
 
         if (isEnvironmentExists(allEnvList,actualEnv)){ // If env is already present just update the project prperies
-			log.info "Environemnt $actualEnv alredy exists"
+            log.info "Environemnt $actualEnv alredy exists"
             project.setActiveEnvironment(actualEnv)
             loadProjectProperties(project,actualEnv);
 
         }
         else  // If env is not present, create new environment and update the project properties
         {
-			log.info "Environemnt $actualEnv NOT exists"
+            log.info "Environemnt $actualEnv NOT exists"
             cloneExistingEnvironment( project,  actualEnv ,  constantEnv)
 
         }
@@ -37,12 +37,12 @@ class ProjectEnvironments {
         def currectEnvironmentServices = getCurrentEnvExistingSoapServices(project,actualEnv)
         addSeviceNameIfNotPresentInEnv(project,currectEnvironmentServices )
 
-		updateAllServicesIfIPAddressPresent(project,actualEnv,constantEnv)
+        updateAllServicesIfIPAddressPresent(project,actualEnv,constantEnv)
         //updateAllServices(project,actualEnv,constantEnv)
         log.info "---->>>>>>>>>>>>>>>>END>>>>>>>>>>>>>>>>>>>>>"
-		
-		log.info "getCurrentEnvExistingSoapServicesDefinition--" + getCurrentEnvExistingSoapServicesDefinition(project,actualEnv)
-		log.info printProjectProperties(project,actualEnv);
+
+        log.info "getCurrentEnvExistingSoapServicesDefinition--" + getCurrentEnvExistingSoapServicesDefinition(project,actualEnv)
+        log.info printProjectProperties(project,actualEnv);
 
     }
 
@@ -55,13 +55,12 @@ class ProjectEnvironments {
     public static boolean loadProjectProperties(def projectTemp, def actualEnvTemp){
         def flag = false;
         try{
-			def file
+            def file
             if (File.separator.equals("\\"))
-            	file= new File(projectTemp.getPath()).getParent().toString() + File.separator + "env" +  File.separator + "env.json"
+                file= new File(projectTemp.getPath()).getParent().toString() + File.separator + actualEnvTemp +  File.separator + "env.json"
             else
-            	file= "/usr/local/SmartBear/project" + File.separator + "env" +  File.separator + "env.json"
-			
-            //def file = new File(projectTemp.getPath()).getParent().toString() + File.separator + "env" +  File.separator + "env.json"
+                file= "/usr/local/SmartBear/project" + File.separator + actualEnvTemp +  File.separator + "env.json"
+
             def jsonSlurper = new groovy.json.JsonSlurper()
             def object = jsonSlurper.parse(new FileReader(new File(file)))
 
@@ -82,16 +81,16 @@ class ProjectEnvironments {
     public static boolean cloneExistingEnvironment(def project, def actualEnv , def constantEnv){
 
         project.setActiveEnvironment(constantEnv)
-		Thread.sleep(1000)
+        Thread.sleep(1000)
         project.getActiveEnvironment().clone(actualEnv)
-		Thread.sleep(1000)
+        Thread.sleep(1000)
         project.setActiveEnvironment(actualEnv)
-		Thread.sleep(1000)
+        Thread.sleep(1000)
         loadProjectProperties(project,actualEnv);
-		Thread.sleep(1000)
-		printProjectProperties(project,actualEnv);
-		
-		log.info "New environment $actualEnv creted"
+        Thread.sleep(1000)
+        printProjectProperties(project,actualEnv);
+
+        log.info "New environment $actualEnv creted"
     }
 
 //    public static boolean updateAllServices(def projectTemp,def actualEnvTemp,def constantEnvTemp){
@@ -127,7 +126,7 @@ class ProjectEnvironments {
 
         def env = projectTemp.getEnvironmentByName(actualEnvTemp);
         def soapServCount = env.getSoapServiceCount()
-		log.info "soapServCount--$soapServCount"
+        log.info "soapServCount--$soapServCount"
         for(int i = 0;i<soapServCount;i++){
             def soapServ = env.getSoapServiceAt(i)
             def endpointConf = soapServ.getEndpoint().getConfig()
@@ -135,39 +134,39 @@ class ProjectEnvironments {
             def isProjectStringPresent = endpointConf.getStringValue().contains("Project")
             log.info "isProjectStringPresent contains /Project/ " + isProjectStringPresent
             //if(!isProjectStringPresent){
-				if(endpointConf.getStringValue().contains("/pin/")){
-					def ipddress = projectTemp.getPropertyValue(actualEnvTemp)
-					log.info "env --$actualEnvTemp && IPAddress -- $ipddress"
-					def endPointStringValuePart2 = endpointConf.getStringValue().split('/pin/')[1]
-					def endPointStringValue = "http://$ipddress/pin/$endPointStringValuePart2"
-					log.info "endPointStringValue_SOAP-->$endPointStringValue"
-					endpointConf.setStringValue(endPointStringValue)
-				}
-                
+            if(endpointConf.getStringValue().contains("/pin/")){
+                def ipddress = projectTemp.getPropertyValue(actualEnvTemp)
+                log.info "env --$actualEnvTemp && IPAddress -- $ipddress"
+                def endPointStringValuePart2 = endpointConf.getStringValue().split('/pin/')[1]
+                def endPointStringValue = "http://$ipddress/pin/$endPointStringValuePart2"
+                log.info "endPointStringValue_SOAP-->$endPointStringValue"
+                endpointConf.setStringValue(endPointStringValue)
+            }
+
 
             //}
 
         }
 
         def restServCount = env.getRestServiceCount()
-		log.info "restServCount--$restServCount"
+        log.info "restServCount--$restServCount"
         for(int i = 0;i<restServCount;i++){
             def restServ = env.getRestServiceAt(i)
             def endpointConf = restServ.getEndpoint().getConfig()
             log.info "endpointConf.getStringValue()-" + endpointConf.getStringValue()
             def isProjectStringPresent = endpointConf.getStringValue().contains("Project")
             log.info "isProjectStringPresent contains /Project/ " + isProjectStringPresent
-           // if(!isProjectStringPresent){
-				if(endpointConf.getStringValue().contains("/pin/")){
-					def ipddress = projectTemp.getPropertyValue(actualEnvTemp)
-					log.info "env --$actualEnvTemp && IPAddress -- $ipddress"
-					def endPointStringValuePart2 = endpointConf.getStringValue().split('/pin/')[1]
-					//def endPointStringValue = "http://\${#Project#${constantEnvTemp}}/pin/" + endPointStringValuePart2
-					def endPointStringValue = "http://$ipddress/pin/$endPointStringValuePart2"
-					log.info "endPointStringValue_REST-->$endPointStringValue"
-					endpointConf.setStringValue(endPointStringValue)
-				}
-                
+            // if(!isProjectStringPresent){
+            if(endpointConf.getStringValue().contains("/pin/")){
+                def ipddress = projectTemp.getPropertyValue(actualEnvTemp)
+                log.info "env --$actualEnvTemp && IPAddress -- $ipddress"
+                def endPointStringValuePart2 = endpointConf.getStringValue().split('/pin/')[1]
+                //def endPointStringValue = "http://\${#Project#${constantEnvTemp}}/pin/" + endPointStringValuePart2
+                def endPointStringValue = "http://$ipddress/pin/$endPointStringValuePart2"
+                log.info "endPointStringValue_REST-->$endPointStringValue"
+                endpointConf.setStringValue(endPointStringValue)
+            }
+
 
             //}
         }
@@ -205,7 +204,7 @@ class ProjectEnvironments {
 
             if(!currectEnvironmentServices.contains(nameofService)){
                 log.info "nameofService $nameofService - Not exists exists"
-               // NewUrl = serviceDefnition
+                // NewUrl = serviceDefnition
                 ServiceImpl soapService = null
                 if(enumType.equals("REST"))
                     soapService = newEnvObject.addNewService(nameofService, com.eviware.soapui.config.ServiceConfig.Type.REST)
@@ -222,45 +221,45 @@ class ProjectEnvironments {
         }
 
     }
-	
-	public static def getCurrentEnvExistingSoapServicesDefinition(def projectTemp, def actualEnvTemp){
+
+    public static def getCurrentEnvExistingSoapServicesDefinition(def projectTemp, def actualEnvTemp){
         def serviceList = []
         def env = projectTemp.getEnvironmentByName(actualEnvTemp);
         //for SOap services
         def soapServCount = env.getSoapServiceCount()
         for(int i = 0;i<soapServCount;i++){
             def soapServ = env.getSoapServiceAt(i)
-			def endpointConf = soapServ.getEndpoint().getConfig()
+            def endpointConf = soapServ.getEndpoint().getConfig()
             serviceList.add(endpointConf.getStringValue())
         }
         //For REST services
         def restServCount = env.getRestServiceCount()
         for(int i = 0;i<restServCount;i++){
             def restServ = env.getRestServiceAt(i)
-			def endpointConf = restServ.getEndpoint().getConfig()
+            def endpointConf = restServ.getEndpoint().getConfig()
             serviceList.add(endpointConf.getStringValue())
         }
         return serviceList
     }
-	
-	public static boolean printProjectProperties(def projectTemp, def actualEnvTemp ){
+
+    public static boolean printProjectProperties(def projectTemp, def actualEnvTemp ){
         def flag = false;
         try{
             def file
             if (File.separator.equals("\\"))
-            	file= new File(projectTemp.getPath()).getParent().toString() + File.separator + "env" +  File.separator + "env.json"
+                file= new File(projectTemp.getPath()).getParent().toString() + File.separator + actualEnvTemp +  File.separator + "env.json"
             else
-            	file= "/usr/local/SmartBear/project" + File.separator + "env" +  File.separator + "env.json"
-			
+                file= "/usr/local/SmartBear/project" + File.separator + actualEnvTemp +  File.separator + "env.json"
+
             log.info "Path --$file"
             def jsonSlurper = new groovy.json.JsonSlurper()
             def object = jsonSlurper.parse(new FileReader(new File(file)))
-			log.info object.environment
+            log.info object.environment
             object.environment.findAll{ it.'NAME' == "$actualEnvTemp" }[0].each {
-                it -> log.info it.key + "|" + it.value 
+                it -> log.info it.key + "|" + it.value
             }
 
-             object.environment.findAll{ it.'NAME' == "$actualEnvTemp" }[0].each {
+            object.environment.findAll{ it.'NAME' == "$actualEnvTemp" }[0].each {
                 it -> log.info "Proj prop--> ${it.key}-->"+ projectTemp.getPropertyValue(it.key)
             }
 
@@ -272,6 +271,6 @@ class ProjectEnvironments {
         return flag;
 
     }
-	
+
 
 }
